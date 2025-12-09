d6e6980008bd0038bf1223b438308ec50b3aa5ec
Hesham-Hamdan <heshamhamdan2018@gmail.com>
2025-12-09 07:31:15 +0200

    fix: fix backend url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Defaults to localhost for dev, but can be overridden by env var
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://13.48.178.53:3000/api";
 
 const client = axios.create({
   baseURL: API_URL,
